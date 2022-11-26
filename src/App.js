d9a0165b6b798788b02fe87f5e905465f59ec29e
Haresh <kuradeharesh4002@gmail.com>
2022-11-26 09:14:32 +0530

    minor changes😜

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import {Routes, Route} from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
 import Card from "./components/Card";
-import CardDetails from './components/CardDetails';
+import CardDetails from "./components/CardDetails";
 
 function App() {
   return (
     <div className="App scrollbar-hide">
       <NavBar />
+      <Card />
       <Routes>
         <Route path="/" element={<Card />} />
         <Route path="/cart/:id" element={<CardDetails />} />
